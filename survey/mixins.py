d078b90c1356579c2fe5e2aa5258ff49300b1a9e
Sebastien Mirolo <smirolo@djaodjin.com>
2016-07-25 21:02:58 -0700

    rewrite get_account to be more generic

--- survey/mixins.py
+++ survey/mixins.py
@@ -22,7 +22,12 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import unicode_literals
+
+from django.core.exceptions import ImproperlyConfigured
+from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext as _
 from django.views.generic.detail import SingleObjectMixin
 
 from . import settings
@@ -31,22 +36,49 @@ from .utils import get_account_model
 
 
 class AccountMixin(object):
-
-    account_kwarg_url = 'organization'
+    """
+    Mixin to use in views that will retrieve an account object (out of
+    ``account_queryset``) associated to a slug parameter (``account_url_kwarg``)
+    in the URL.
+    The ``account`` property will be ``None`` if either ``account_url_kwarg``
+    is ``None`` or absent from the URL pattern.
+    """
+    account_queryset = get_account_model().objects.all()
+    account_lookup_field = settings.ACCOUNT_LOOKUP_FIELD
+    account_url_kwarg = 'organization'
 
     @property
     def account(self):
         if not hasattr(self, '_account'):
-            self._account = self.get_account()
+            if (self.account_url_kwarg is not None
+                and self.account_url_kwarg in self.kwargs):
+                if self.account_queryset is None:
+                    raise ImproperlyConfigured(
+                        "%(cls)s.account_queryset is None. Define "
+                        "%(cls)s.account_queryset." % {
+                            'cls': self.__class__.__name__
+                        }
+                    )
+                if self.account_lookup_field is None:
+                    raise ImproperlyConfigured(
+                        "%(cls)s.account_lookup_field is None. Define "
+                        "%(cls)s.account_lookup_field as the field used "
+                        "to retrieve accounts in the database." % {
+                            'cls': self.__class__.__name__
+                        }
+                    )
+                kwargs = {'%s__exact' % self.account_lookup_field:
+                    self.kwargs.get(self.account_kwarg_url)}
+                try:
+                    self._account = self.account_queryset.filter(**kwargs).get()
+                except self.account_queryset.model.DoesNotExist:
+                    #pylint: disable=protected-access
+                    raise Http404(_("No %(verbose_name)s found matching"\
+                        "the query") % {'verbose_name':
+                        self.account_queryset.model._meta.verbose_name})
+            else:
+                self._account = None
         return self._account
-
-    def get_account(self):
-        if self.kwargs.has_key(self.account_kwarg_url):
-            account_model = get_account_model()
-            kwargs = {'%s__exact' % settings.ACCOUNT_LOOKUP_FIELD:
-                self.kwargs.get(self.account_kwarg_url)}
-            return get_object_or_404(account_model, **kwargs)
-        return None
 
     def get_context_data(self, *args, **kwargs):
         context = super(AccountMixin, self).get_context_data(*args, **kwargs)
