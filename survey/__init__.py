e33f70a36930c91e4c21aabd5611115258a163ca
Sebastien Mirolo <smirolo@djaodjin.com>
2017-04-09 23:11:54 -0700

    release 0.1.6 to pypi

--- survey/__init__.py
+++ survey/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, Djaodjin Inc.
+# Copyright (c) 2017, Djaodjin Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,4 +26,4 @@
 PEP 386-compliant version number for the survey django app.
 """
 
-__version__ = '0.1.6-dev'
+__version__ = '0.1.6'
